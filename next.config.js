7e769305a5dc9823bfe8cdc99e17cca88ab64256
Nurullah Gumus <nurullah.gumus@heliocampus.com>
2023-12-21 22:29:42 +0100

    fix deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withNextra = require("nextra")({
 });
 
 const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/blog" : "";
+const assetPrefix = isProduction ? "/blog" : "/blog";
 
 const nextConfig = {
   images: {
